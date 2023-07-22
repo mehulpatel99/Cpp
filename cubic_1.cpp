bd4660716c8fb74e37e5198e1e72e483cd8fd243
mehulpatel99 <138190866+mehulpatel99@users.noreply.github.com>
2023-07-22 11:51:15 +0530

    Add files via upload

---
+++ cubic_1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+//class cube
+//{
+			int num1,num2;
+//	public:
+		inline multi()
+		{
+			cout<<"enter your no."<<endl;
+			cin>>num1>>num2;
+			cout<<"your multiply no is."<<num1*num2<<endl;
+		}
+		inline cubic()
+		{
+		
+			cout<<"your cubic value "<<num1<<" is :"<<num1*num1*num1<<endl;
+			cout<<"your cubic value "<<num2<<" is :"<<num2*num2*num2<<endl;
+		}
+//};
+int main()
+{
+//	cube s;
+
+	multi();
+	cubic();
+
+
+}
