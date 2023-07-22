78404c8173fa7835f8b69819e6aa78e206d3362c
mehulpatel99 <138190866+mehulpatel99@users.noreply.github.com>
2023-07-22 11:51:39 +0530

    Add files via upload

---
+++ constructor_cal.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//class calculator
+class s
+{
+	public:
+   s()
+	{
+	int i,a;
+
+//	void add()
+	 cout << "Enter add numbers: "<<endl;
+    cin>>i >>a;
+		   cout <<"add is :" <<i + a << endl<<endl;
+		
+		//	void sub()
+    
+		cout<<"sub is :"<<i-a<<endl<<endl;
+	
+//	void multiply()
+    
+	
+		cout<<"multiply is :"<<i*a<<endl<<endl;
+	
+//	void divide()
+    
+		 
+		cout<<"divide is: "<<i/a<<endl<<endl;
+		
+	
+	}
+
+};
+
+int main()
+{
+
+    s pri;
+    
+}
